018ff9715670f007daf8b8ebabe62e837a147060
24331a05i5-tech <24331a05i5@mvgrce.edu.in>
2025-12-17 20:54:58 +0530

    Add files via upload

---
+++ 4th.py
@@ -0,0 +1,8 @@
+import math
+print("Pi value is: ",math.pi)
+print("Square root of 9 is: ",math.sqrt(9))
+print("Sin of 0 is: ",math.sin(0))
+print("cos of 0 is: ",math.cos(0))
+print("Sin of 90 is: ",math.sin(math.pi/2))
+print("cos of 60 is: ",math.cos(math.pi/3))
+print("tan of 45 is: ",math.tan(math.pi/4))
